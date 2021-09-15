de4a69a7817c0a3839b28c354a59bb801f39f1ff
Andrey Kim <akart.dev@gmail.com>
2021-09-16 01:59:47 +0600

    first commit

---
+++ CoreData-projectOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-projectOne
+//
+//  Created by Andrey Kim on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
